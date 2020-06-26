6240dce957ea0a41f5ab48f1674ae10e14592331
akash <akasky70@gmail.com>
2020-06-26 15:53:23 +0545

    add footer

---
+++ src/auth/component/signin.component.tsx
@@ -0,0 +1,94 @@
+import React, { useContext, useReducer, useState, useEffect } from 'react';
+
+import * as auth from 'auth/auth.state';
+import { Action } from 'auth/auth.type';
+import { signIn } from 'api/resource.api';
+import { ApiResponse } from 'api/api.type';
+import { AuthContext } from 'auth/auth.context';
+import { securedLS } from 'helper/local-storage-helper';
+
+import { Input } from 'ui/form/input';
+import { Button } from 'ui/form/button';
+import { ErrorAlert } from 'ui/alert/inline-alert';
+
+const handleSignIn = async (
+  event: any,
+  dispatch: (props: any) => void,
+  setSignInError: (error: string) => void
+) => {
+  event.preventDefault();
+  dispatch({ type: auth.SIGN_IN_PENDING });
+
+  const { data, error } = await signIn({
+    email: event.target[0].value,
+    password: event.target[1].value,
+  });
+
+  if (error) {
+    setSignInError(error.message);
+    return dispatch({ type: auth.SIGN_IN_ERROR });
+  }
+
+  dispatch({
+    type: auth.SIGN_IN_SUCCESS,
+    payload: { token: data.token, roles: data.roles },
+  });
+};
+
+const restoreAuthentication = (dispatch: (props: Action) => void) => {
+  dispatch({ type: auth.SIGN_IN_PENDING });
+  const { data }: ApiResponse = securedLS.get(auth.AUTH_LS_KEY);
+
+  if (data) {
+    return dispatch({ type: auth.RESTORE_AUTH, payload: data });
+  }
+
+  dispatch({ type: auth.SIGN_IN_ERROR });
+};
+
+const SigninForm = () => {
+  const [checkAuth, setCheckAuth] = useState<boolean>(true);
+  const [signInError, setSignInError] = useState<String>('');
+  const { setCurrentAuth, isHandlingAuth } = useContext(AuthContext);
+  const [authState, dispatch] = useReducer(auth.reducer, auth.initialState);
+
+  useEffect(() => {
+    setCurrentAuth(authState);
+
+    if (checkAuth) {
+      setCheckAuth(false);
+      restoreAuthentication(dispatch);
+    }
+  }, [authState, checkAuth, setCheckAuth, setCurrentAuth]);
+
+  return (
+    <form
+      className="col-12 p-md-3 p-0"
+      onChange={() => setSignInError('')}
+      onSubmit={(e) => handleSignIn(e, dispatch, setSignInError)}
+    >
+      <ErrorAlert message={signInError} />
+      <Input
+        type="email"
+        name="email"
+        required={true}
+        placeholder="Email"
+        className={`${signInError ? 'is-invalid ' : ''}`}
+      />
+      <Input
+        type="password"
+        name="password"
+        required={true}
+        placeholder="Password"
+        className={`${signInError ? 'is-invalid ' : ''}`}
+      />
+      <Button
+        name="Sign in"
+        disabled={isHandlingAuth}
+        className="md btn-primary"
+      />
+    </form>
+  );
+};
+
+export default SigninForm;
--- src/ui/layout/plain-background.layout.tsx
+++ src/ui/layout/plain-background.layout.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { LayoutProps } from './layout.type';
 import PublicNavBar from './component/public-navbar';
+import Footer from './component/footer';
 
 const PlainBackground = ({ children, className = '' }: LayoutProps) => {
   return (
     <div className={`col-md-11 m-auto ${className}`}>
       <PublicNavBar />
       <div className="bring-to-front">{children}</div>
+      <Footer />
     </div>
   );
 };
