736c96f0c9b09f9ce13d8264f4dc4fb850da83fb
Bikash Gurung <bikashtamu99@gmail.com>
2020-06-28 11:14:31 +0545

    Updates

---
+++ precache-manifest.589f14e58a6064aaf6c37e1ebf549859.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69edec6fce83e468178f3a0e6537c5a",
+    "url": "/educere-frontend/index.html"
+  },
+  {
+    "revision": "9d4f4e7939d74b0b718a",
+    "url": "/educere-frontend/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "c44c573f3a8289030a77",
+    "url": "/educere-frontend/static/css/main.a94526b1.chunk.css"
+  },
+  {
+    "revision": "9d4f4e7939d74b0b718a",
+    "url": "/educere-frontend/static/js/2.e13f2e83.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/educere-frontend/static/js/2.e13f2e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44c573f3a8289030a77",
+    "url": "/educere-frontend/static/js/main.20dca847.chunk.js"
+  },
+  {
+    "revision": "4f636e116dc59a823e74",
+    "url": "/educere-frontend/static/js/runtime-main.ab29549f.js"
+  },
+  {
+    "revision": "d606287145b96e70de63ef69269269e4",
+    "url": "/educere-frontend/static/media/blue-spinner.d6062871.svg"
+  },
+  {
+    "revision": "fb05bb017491a1c57ba9f3cd80d1b261",
+    "url": "/educere-frontend/static/media/dotted-loader.fb05bb01.svg"
+  },
+  {
+    "revision": "5e110b921a8165660d4df6ccf9ca2f48",
+    "url": "/educere-frontend/static/media/info-icon.5e110b92.svg"
+  }
+]);
